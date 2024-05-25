7e5d0ab57e9403ccacc5de20dc08a4a88a58ccee
Ruedi Steinmann <ruediste@gmail.com>
2024-05-25 21:09:22 +0200

    work on graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,106 @@
-import { useCallback } from 'react';
+import { useCallback, useRef } from "react";
 import ReactFlow, {
   Background,
   BackgroundVariant,
   Connection,
+  ConnectionMode,
   Controls,
+  Handle,
   MiniMap,
+  NodeProps,
+  NodeTypes,
+  Position,
+  ReactFlowInstance,
+  ReactFlowProvider,
   addEdge,
   useEdgesState,
   useNodesState,
-} from 'reactflow';
- 
-import 'reactflow/dist/style.css';
- 
+} from "reactflow";
+
+import "reactflow/dist/style.css";
+import Sidebar from "./Sidebar";
+
+let id = 0;
+function getId() {
+  return `dndnode_${id++}`;
+}
+
+function NumberNode({ data }: NodeProps<{ label: string }>) {
+  return (
+    <>
+      {data.label}
+      <Handle type="source" position={Position.Right} />
+    </>
+  );
+}
+
+const nodeTypes: NodeTypes = {
+  number: NumberNode,
+};
+
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
- 
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
- 
+  const reactFlowInstance = useRef<ReactFlowInstance>();
+
   const onConnect = useCallback(
     (connection: Connection) => setEdges((eds) => addEdge(connection, eds)),
-    [setEdges],
+    [setEdges]
   );
- 
+
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-      >
-        <Controls />
-        <MiniMap />
-        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
-      </ReactFlow>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+      }}
+    >
+      <ReactFlowProvider>
+        <div style={{ flexGrow: 1 }}>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            nodeTypes={nodeTypes}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onInit={(instance) => (reactFlowInstance.current = instance)}
+            connectionMode={ConnectionMode.Loose}
+            onDragOver={(event) => {
+              event.preventDefault();
+              event.dataTransfer.dropEffect = "move";
+            }}
+            onDrop={(event) => {
+              event.preventDefault();
+              const type = event.dataTransfer.getData("application/reactflow");
+              const position = reactFlowInstance.current!.screenToFlowPosition({
+                x: event.clientX,
+                y: event.clientY,
+              });
+              setNodes((ns) =>
+                ns.concat({
+                  id: getId(),
+                  type,
+                  position,
+                  data: { label: type },
+                })
+              );
+            }}
+          >
+            <Controls />
+            <MiniMap />
+            <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+          </ReactFlow>
+        </div>
+        <Sidebar />
+      </ReactFlowProvider>
     </div>
   );
 }
---
+++ src/Sidebar.tsx
@@ -0,0 +1,41 @@
+export default () => {
+  const onDragStart = (
+    event: React.DragEvent<HTMLDivElement>,
+    nodeType: string
+  ) => {
+    event.dataTransfer.setData("application/reactflow", nodeType);
+    event.dataTransfer.effectAllowed = "move";
+  };
+
+  return (
+    <aside>
+      <div className="description">
+        You can drag these nodes to the pane on the right.
+      </div>
+      <div
+        className="dndnode input"
+        onDragStart={(event) => onDragStart(event, "input")}
+        draggable
+      >
+        Input Node
+      </div>
+      <div
+        className="dndnode"
+        onDragStart={(event) => onDragStart(event, "default")}
+        draggable
+      >
+        Default Node
+      </div>
+      <div
+        className="dndnode output"
+        onDragStart={(event) => onDragStart(event, "output")}
+        draggable
+      >
+        Output Node
+      </div>
+      <div onDragStart={(event) => onDragStart(event, "number")} draggable>
+        Number
+      </div>
+    </aside>
+  );
+};
